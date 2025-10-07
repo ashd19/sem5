b08344b1834dc5801f6dd9300a1e63de17dd5834
Ashton <ashtondsouza192@gmail.com>
2025-10-07 19:25:27 +0530

    cleanup

--- a9/9.ts
+++ a9/9.ts
@@ -1,5 +1,5 @@
-import {promises as fs}  from "fs";
-import {resolve} from 'path';
+import { promises as fs } from "fs";
+import { resolve } from "path";
 
 // promise is an object which contains the status of completion of an operation
 // A promise has 3 states:
@@ -10,30 +10,24 @@ import {resolve} from 'path';
 // We use await to wait for the promise to resolve and get the actual result
 // Without await, we get the promise object itself, not the data it will eventually contain
 
-
-
-
-// 1. loading data from a large data file 
+// 1. loading data from a large data file
 //read the content  , utf-8 encoding is just another way to convert the raw bytes into string.
 // .readFile is async , it takes time to read the file , Code pauses at await, waits for file to be read completely, then continues.
-// without await you get the promise not the data 
-// 
-async function getData(){
-  console.time('getData');
-  const dataFile  =  resolve('data.json');
-  const content  = await fs.readFile(dataFile,'utf-8');
+// without await you get the promise not the data
+//
+async function getData() {
+  console.time("getData");
+  const dataFile = resolve("data.json");
+  const content = await fs.readFile(dataFile, "utf-8");
   console.log(content);
 
-  console.timeEnd('getData');
-
+  console.timeEnd("getData");
 }
 getData();
-
 
 // 2.timeout function and clear ??
 
 
-// 3. getting data from an external api 
 
 
-
+// 3. getting data from an external api
